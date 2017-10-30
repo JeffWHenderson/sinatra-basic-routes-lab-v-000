cc68b7eace45e92b0291a7459f3677acfbe732f0
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-30 21:46:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    puts "My name is #{name}"
+    puts "My name is Jeff"
   end
 
   get '/hometown' do
-    puts "My hometown is "
+    puts "My hometown is Floyds Knobs, IN"
   end
 
   get '/favorite-song' do
-    puts "My favorite song is "
+    puts "My favorite song is The Less I Know The Better"
   end
 end
