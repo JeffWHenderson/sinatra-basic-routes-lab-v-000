c9db624662c1328160686d09f63bffe4f58e19a3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-30 21:43:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,5 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/name' do
     puts "hello"
-  end 
+  end
+
+  get '/name' do
+    puts "hello"
+  end
+
+  get '/name' do
+    puts "hello"
+  end
 end
